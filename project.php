050dc188a9c86353b886febc9f33da501bbcbb1d
Kim Nguyen <design@kimisgold.com>
2016-06-03 14:30:22 -0400

    Set up individual project layout.

---
+++ project.php
@@ -0,0 +1,35 @@
+<?php /* Template Name: Project */ ?>
+
+<?php $customFields = get_post_custom(); ?>
+
+<?php define( 'WP_USE_THEMES', false ); get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div id="intro" <?php echo ($imgBgAttr) ? $imgBgAttr : ''; ?>>
+    <div class="container">
+    <?php if ($introTitle = $customFields['Intro title'][0]): ?>
+    <h1><?php echo $introTitle; ?></h1>
+    <?php else: ?>
+    <h1><?php echo the_title(); ?></h1>
+    <?php endif; ?>
+    <?php if ($introText = $customFields['Intro text'][0]): ?>
+    <p><?php echo $introText; ?></p>
+    <?php endif; ?>
+    </div>
+</div>
+
+<div class="container">
+    <aside id="project-meta">
+        <?php the_category(); ?>
+    </aside>
+    <article id="project-description">
+        <?php echo the_content(); ?>
+    </article>
+</div>
+
+<?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
