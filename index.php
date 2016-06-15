ed50d5f5a6acf372a246e3a30da3b471ab68d602
Kim Nguyen <design@kimisgold.com>
2016-06-15 17:04:06 -0400

    Set up news section.

--- index.php
+++ index.php
@@ -2,27 +2,39 @@
 
 <?php define( 'WP_USE_THEMES', false ); get_header(); ?>
 
+<div class="container">
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<div id="intro" <?php echo ($imgBgAttr) ? $imgBgAttr : ''; ?>>
-    <div class="container">
-    <?php if ($introTitle = $customFields['Intro title'][0]): ?>
-    <h1><?php echo $introTitle; ?></h1>
-    <?php else: ?>
-    <h1><?php echo the_title(); ?></h1>
-    <?php endif; ?>
-    <?php if ($introText = $customFields['Intro text'][0]): ?>
-    <p><?php echo $introText; ?></p>
-    <?php endif; ?>
+    <div class="post">
+        <aside class="post-meta">
+            <span class="date"><?php the_date('n/j/Y'); ?></span>
+            <?php if (get_the_author_meta('user_firstname')): ?>
+                <?php $authorID = get_the_author_id(); ?>
+                <?php $authorUrl = get_author_posts_url($authorID); ?>
+                <?php if (function_exists('get_cimyFieldValue') && get_cimyFieldValue($authorID, 'picture')): ?>
+                    <?php $avatar = get_cimyFieldValue($authorID, 'picture'); ?>
+                <?php else: ?>
+                    <?php $avatar = get_bloginfo('template_directory') . '/img/blank_staff.png'; ?>
+                <?php endif; ?>
+                <a href="<?php echo $authorUrl; ?>" class="avatar"><img src="<?php echo $avatar; ?>" title="avatar for <?php echo $authorName; ?>"></a>
+                <span class="author"><a href="<?php echo $authorUrl; ?>"><?php the_author_meta('user_firstname'); ?> <?php the_author_meta('user_lastname'); ?></a></span>
+            <?php endif; ?>
+        </aside>
+        <article>
+        <h1><?php the_title(); ?></h1>
+        <?php echo the_content(); ?>
+        </article>
     </div>
-</div>
-
-<div class="container">
-    <?php echo the_content(); ?>
-</div>
 
 <?php endwhile; else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
+
+<nav class="pagination">
+    <?php echo paginate_links(); ?>
+</nav>
+
+</div>
 
 <?php get_footer(); ?>
