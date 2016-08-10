57890a55c37049353b6c914532ca57e323d9b7bb
Kim Nguyen <design@kimisgold.com>
2016-08-10 13:20:00 -0400

    Move features into main project area on What We Do.

--- projects.php
+++ projects.php
@@ -53,32 +53,6 @@ $featured = get_posts($featuredFilters);
 
 <div id="content">
     <?php echo the_content(); ?>
-    <div id="featured">
-        <h3>Featured Projects</h3>
-        <?php foreach ($featured as $featuredProject): ?>
-            <?php
-            $projectID = $featuredProject->ID;
-            $projectMeta = get_post_custom($projectID);
-            $imgBgUrl = '';
-            ?>
-            <div class="project">
-                <?php
-                $imgBgUrl = '';
-                if ( has_post_thumbnail($projectID) ) {
-                    $imgBgUrl = wp_get_attachment_image_src( get_post_thumbnail_id($projectID), 'large' );
-                    $imgBgUrl = $imgBgUrl[0];
-                } else if (isset($projectMeta['Image'])) {
-                    $imgBgUrl = site_url() . '/ui/i/project-images/' . $projectMeta['Image'][0];
-                }
-                ?>
-                <a href="<?php echo esc_url(get_permalink($projectID)); ?>" class="thumbnail" style="background-image:url('<?php echo $imgBgUrl; ?>')"></a>
-                <h4><a href="<?php echo esc_url(get_permalink($projectID)); ?>"><?php echo $featuredProject->post_title; ?></a></h4>
-                <?php if (isset($projectMeta['Short Description'])): ?>
-                <?php echo $projectMeta['Short Description'][0]; ?>
-                <?php endif; ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
     <nav>
         <h2>Browse Projects</h2>
         <ul>
@@ -96,6 +70,33 @@ $featured = get_posts($featuredFilters);
         </ul>
     </nav>
     <div id="projects">
+        <div id="featured">
+            <h3>Featured Projects</h3>
+            <?php foreach ($featured as $featuredProject): ?>
+                <?php
+                $projectID = $featuredProject->ID;
+                $projectMeta = get_post_custom($projectID);
+                $imgBgUrl = '';
+                ?>
+                <div class="project">
+                    <?php
+                    $imgBgUrl = '';
+                    if ( has_post_thumbnail($projectID) ) {
+                        $imgBgUrl = wp_get_attachment_image_src( get_post_thumbnail_id($projectID), 'large' );
+                        $imgBgUrl = $imgBgUrl[0];
+                    } else if (isset($projectMeta['Image'])) {
+                        $imgBgUrl = site_url() . '/ui/i/project-images/' . $projectMeta['Image'][0];
+                    }
+                    ?>
+                    <a href="<?php echo esc_url(get_permalink($projectID)); ?>" class="thumbnail" style="background-image:url('<?php echo $imgBgUrl; ?>')"></a>
+                    <h4><a href="<?php echo esc_url(get_permalink($projectID)); ?>"><?php echo $featuredProject->post_title; ?></a></h4>
+                    <?php if (isset($projectMeta['Short Description'])): ?>
+                    <?php echo $projectMeta['Short Description'][0]; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
         <?php foreach ($projects as $project): ?>
             <?php
             $projectID = $project->ID;
