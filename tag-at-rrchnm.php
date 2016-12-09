5687eb2ac470e1165dd713acee119f24813f00ef
Kim Nguyen <design@kimisgold.com>
2016-12-09 18:57:07 -0500

    Add event tag templates and links in events archive.

--- tag-at-rrchnm.php
+++ tag-at-rrchnm.php
@@ -1,5 +1,6 @@
 <?php
 $customFields = get_post_custom();
+$eventCategory = get_term_by('slug', 'events', 'category');
 $rrchnmAtTag = get_term_by('slug', 'rrchnm-at', 'post_tag');
 $atRrchnmTag = get_term_by('slug', 'at-rrchnm', 'post_tag');
 ?>
@@ -9,10 +10,10 @@ $atRrchnmTag = get_term_by('slug', 'at-rrchnm', 'post_tag');
 <div class="container">
 
 <aside id="blog-meta">
-    <h1>Events</h1>
+    <h1>Events: @RRCHNM</h1>
 
     <a href="<?php echo get_tag_link($rrchnmAtTag->term_id); ?>" class="rrchnm-at tag">RRCHNM@</a>
-    <a href="<?php echo get_tag_link($atRrchnmTag->term_id); ?>" class="at-rrchnm tag">@RRCHNM</a>
+    <a href="<?php echo get_category_link($eventCategory->term_id); ?>" class="back-link">Back to all events</a>
 </aside>
 
 <?php include('event-posts.php'); ?>
