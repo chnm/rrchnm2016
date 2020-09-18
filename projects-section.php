3cbab80ff62573b79d388c05dbf2693ab8da3865
Kim Nguyen <design@kimisgold.com>
2020-09-18 15:22:49 -0400

    Only show project section if it has projects.

--- projects-section.php
+++ projects-section.php
@@ -1,7 +1,10 @@
-<div class="section">
+<?php $projects = $args['projects']; ?>
+<?php if (count($projects) > 0): ?>
+ <div class="section">
     <h3><?php echo $args['title']; ?></h3>
-    <?php foreach ($args['projects'] as $project): ?>
+    <?php foreach ($projects as $project): ?>
       <?php $projectID = $project->ID; ?>
       <?php get_template_part('projects-single', null, array('projectID' => $projectID)); ?>
     <?php endforeach; ?>
 </div>
+<?php endif; ?>
