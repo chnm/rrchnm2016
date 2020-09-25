242b132e1fa75db56dd35c8907eb181747119976
Kim Nguyen <design@kimisgold.com>
2020-09-25 11:45:00 -0400

    Provide accesible name and presentation role to project images. (#80)

--- projects-single.php
+++ projects-single.php
@@ -3,6 +3,7 @@
     $imgBgUrl = '';
     $projectID = $args['projectID'];
     $projectMeta = get_post_custom($projectID);
+    $projectTitle = get_the_title($projectID);
     if ( has_post_thumbnail($projectID) ) {
         $imgBgUrl = wp_get_attachment_image_src( get_post_thumbnail_id($projectID), 'large' );
         $imgBgUrl = $imgBgUrl[0];
@@ -10,8 +11,8 @@
         $imgBgUrl = site_url() . '/ui/i/project-images/' . $projectMeta['Image'][0];
     }
     ?>
-    <a href="<?php echo esc_url(get_permalink($projectID)); ?>" class="thumbnail" style="background-image:url('<?php echo $imgBgUrl; ?>')"></a>
-    <h4><a href="<?php echo esc_url(get_permalink($projectID)); ?>"><?php echo get_the_title($projectID); ?></a></h4>
+    <a href="<?php echo esc_url(get_permalink($projectID)); ?>" class="thumbnail" style="background-image:url('<?php echo $imgBgUrl; ?>')" role="presentation">Logo for <?php echo $projectTitle; ?></a>
+    <h4><a href="<?php echo esc_url(get_permalink($projectID)); ?>"><?php echo $projectTitle; ?></a></h4>
     <?php if (isset($projectMeta['Short Description']) && ($args['isFeatured'] == true)): ?>
     <?php echo $projectMeta['Short Description'][0]; ?>
     <?php endif; ?>
