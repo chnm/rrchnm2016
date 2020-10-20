2a818b3692524225dc5a10b44212cb0c4269f9d6
Kim Nguyen <design@kimisgold.com>
2020-10-20 14:05:33 -0400

    Use template part with category template.

--- category.php
+++ category.php
@@ -72,21 +72,8 @@ $whatWeDoPageID = $whatWeDoPage->ID;
             <?php while ($projectQuery->have_posts()) : $projectQuery->the_post(); ?>
                 <?php
                 $projectID = get_the_ID();
-                $projectMeta = get_post_custom($projectID);
-                $imgBgUrl = '';
+                get_template_part('projects-single', null, array('projectID' => $projectID, 'isFeatured' => false));
                 ?>
-                <div class="project">
-                    <?php
-                    if ( has_post_thumbnail($projectID) ) {
-                        $imgBgUrl = wp_get_attachment_image_src( get_post_thumbnail_id($projectID), 'large' );
-                        $imgBgUrl = $imgBgUrl[0];
-                    } else if (isset($projectMeta['Image'])) {
-                        $imgBgUrl = site_url() . '/ui/i/project-images/' . $projectMeta['Image'][0];
-                    }
-                    ?>
-                    <a href="<?php echo esc_url(get_permalink($projectID)); ?>" class="thumbnail" style="background-image:url('<?php echo $imgBgUrl; ?>')"></a>
-                    <h4><a href="<?php echo esc_url(get_permalink($projectID)); ?>"><?php echo the_title(); ?></a></h4>
-                </div>
             <?php endwhile; ?>
             </div>
         </div>
