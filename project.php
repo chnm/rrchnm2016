42ec63403a9b0c716caf62f46a476fef3e7390f5
Kim Nguyen <design@kimisgold.com>
2021-02-05 12:22:43 -0500

    Remove hardcoded http:// prefix.

--- project.php
+++ project.php
@@ -18,12 +18,10 @@
 <div class="container">
     <aside id="project-meta">
         <?php if ($projectURL = $customFields['URL'][0]): ?>
-            <?php if (!strpos($projectURL, 'http://')): ?>
-            <?php $projectURL = 'http://' . $projectURL; ?>
-            <?php endif; ?>
+
             <div id="project-website" class="meta-field">
                 <h3>Website</h3>
-                <a href="<?php echo $projectURL; ?>"><?php echo $projectURL; ?></a>
+                <a href="<?php echo '//' . $projectURL; ?>"><?php echo $projectURL; ?></a>
             </div>
         <?php endif; ?>
         <div id="project-contributors" class="meta-field">
